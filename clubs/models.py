ddcea9ab39912433bc84a285fa466f5645d2dd32
Ruslan Nišajev <ruslannisajev@MacBook-Pro-Ruslan.local>
2025-11-05 15:44:38 +0200

    added the database model and database table

--- clubs/models.py
+++ clubs/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Club(models.Model):
+    name = models.CharField(max_length=100)
+    games = models.IntegerField(default=0)
+    wins = models.IntegerField(default=0)
+    draws = models.IntegerField(default=0)
+    wins = models.IntegerField(default=0)
+    losses = models.IntegerField(default=0)
+
+    def win_percentage(self):
+        return round(self.wins / self.games * 100, 1) if self.games else 0
+    
+    def draw_percentage(self):
+        return round(self.draws / self.games * 100, 1) if self.games else 0   
+    
+    def loss_percentage(self):
+        return round(self.losses / self.games * 100, 1) if self.games else 0
+
