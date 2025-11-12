159c2f87d4b1411359243fc68f4f515ffaba1a8a
Ruslan Nišajev <ruslannisajev@MacBook-Pro-Ruslan.local>
2025-11-12 16:39:25 +0200

    added the information about our project into views.py in main

--- main/views.py
+++ main/views.py
@@ -1,3 +1,14 @@
+"""
+Projekt: Jalgpalliklubide statistika
+Autorid: Konstantin Geimonen ja Ruslan Nišajev
+Kirjeldus: Django veebiprojekt, mis kuvab jalgpalliklubide viimase viie mängu statistikat 
+(näiteks võitude, viikide ja kaotuste arv ning nende protsentuaalne jaotus). 
+Praegu on süsteemis olemas ainult Borussia Dortmundi andmed.
+Kasutatud allikad:
+https://youtu.be/qz0aGYrrlhU?si=ob0dm4BXOOYvvNGz
+https://youtu.be/rHux0gMZ3Eg?si=zQLUXl79b16COGoq
+"""
+
 from django.shortcuts import render
 from django.http import HttpResponse
 import requests
